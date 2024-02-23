df3786cc6441666820c5f0d20f91749eb37423c6
Karol Kotański <karolfm12@onet.eu>
2024-02-23 14:47:12 +0100

    ok

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("hello");
+const faqlist = document.querySelector(".faq__list");
+const faqListItems = document.querySelectorAll(".faq__list--item");
+
+faqlist.addEventListener("click", (e) => {
+  const clicked = e.target.firstElementChild;
+  faqListItems.forEach((val) => {
+    if (val.firstElementChild !== clicked) {
+      val.firstElementChild.classList.remove("active");
+    }
+  });
+  clicked.classList.toggle("active");
+});
