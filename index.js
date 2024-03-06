1b9d2949c47cb9f7017e564a77a6d83cf21888c1
Karol Kotański <karolfm12@onet.eu>
2024-03-06 14:36:24 +0100

    NEXTtRY

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ faqlist.addEventListener("click", (e) => {
   // console.log(e.target.parentElement);
   const clicked = e.target.parentElement.lastElementChild;
   const arrow = e.target.parentElement.children[0].children[0];
-
+  // console.log(clicked);
   if (clicked.classList.contains("faq__list--item__paragraph")) {
     faqListItems.forEach((el) => {
-      console.log(clicked);
-      // if (el.children[1].classList.contains("active")) {
       el.children[1].classList.remove("active");
-      // el.classList.remove("active");
-      // }
+      el.children[0].classList.remove("active-main-text");
+      el.children[0].children[0].classList.remove("arrow-active");
     });
 
     clicked.classList.toggle("active");
